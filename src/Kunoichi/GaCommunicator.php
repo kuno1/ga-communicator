41f62788c58cbeb83d53afdfcd5a6af13b734a40
fumikito <guy@hametuha.com>
2020-06-04 15:41:39 +0900

    Add initial files.

---
+++ src/Kunoichi/GaCommunicator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kunoichi;
+
+
+use Hametuha\SingletonPattern\Singleton;
+
+/**
+ * Google Analytics Communicator
+ *
+ * @package ga-communicator
+ */
+class GaCommunicator extends Singleton {
+
+
+
+}
