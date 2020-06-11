9122bf3ceba578f26ca33b2624b8478be2756139
fumikito <guy@hametuha.com>
2020-06-11 14:27:37 +0900

    Add github actions.

---
+++ bin/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf ./.git
+rm -rf ./.github
+rm -rf ./bin
+rm -rf ./tests
+rm -rf ./vendor
+rm -rf ./wp
+rm -rf ./.gitattributes
+rm -rf ./.gitignore
+rm -rf ./.wp-env.json
+rm -rf ./composer.lock
+rm -rf ./package-lock.json
+rm -rf ./phpunit.xml.dit
